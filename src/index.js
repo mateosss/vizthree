f975f8ae0b6247b1e854c361893ec4d8dd2ccb83
Mateo de Mayo <mateodemayo@gmail.com>
2021-08-04 21:36:47 -0300

    Refactor Game class to better reflect future usage

--- src/index.js
+++ src/index.js
@@ -1,34 +1,42 @@
 // import * as THREE from 'three';
-import * as THREE from "https://cdn.skypack.dev/pin/three@v0.131.1-ABR1EJL0AQkCASkHoEad/mode=imports,min/optimized/three.js";
-
-const scene = new THREE.Scene();
-const camera = new THREE.PerspectiveCamera(
-  75,
-  window.innerWidth / window.innerHeight,
-  0.1,
-  1000
-);
-const renderer = new THREE.WebGLRenderer();
-renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
-
-const geometry = new THREE.BoxGeometry();
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-const cube = new THREE.Mesh(geometry, material);
-scene.add(cube);
-
-camera.position.z = 5;
-
-function animate() {
-  requestAnimationFrame(animate);
-  cube.rotation.x += 0.01;
-  cube.rotation.y += 0.01;
-  renderer.render(scene, camera);
-}
-animate();
+import * as THREE from "https://cdn.skypack.dev/pin/three@v0.131.1-ABR1EJL0AQkCASkHoEad/mode=imports,min/optimized/three.js"
 
 export default class Game {
   constructor() {
-    console.log("Constructed!");
+    this.scene = new THREE.Scene()
+    this.camera = new THREE.PerspectiveCamera(
+      75,
+      window.innerWidth / window.innerHeight,
+      0.1,
+      1000
+    )
+    this.renderer = new THREE.WebGLRenderer()
+
+    this.renderer.setSize(window.innerWidth, window.innerHeight)
+    document.body.appendChild(this.renderer.domElement)
+    this.addCube()
+    this.camera.position.z = 5
+  }
+
+  addCube() {
+    const geometry = new THREE.BoxGeometry()
+    const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
+    const cube = new THREE.Mesh(geometry, material)
+    this.scene.add(cube)
+    this.cube = cube
+  }
+
+  start() {
+    this.update()
+  }
+
+  update() {
+    requestAnimationFrame(() => this.update())
+    this.cube.rotation.x += 0.01
+    this.cube.rotation.y += 0.01
+    this.renderer.render(this.scene, this.camera)
   }
 }
+
+const game = new Game()
+game.start()
