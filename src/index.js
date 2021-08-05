831faee2f01763e362c7ffb70062ebd5e71062e5
Mateo de Mayo <mateodemayo@gmail.com>
2021-08-04 22:26:30 -0300

    Add Cube and Line classes

--- src/index.js
+++ src/index.js
@@ -1,51 +1,67 @@
 // import * as THREE from 'three';
 import * as THREE from "https://cdn.skypack.dev/pin/three@v0.131.1-ABR1EJL0AQkCASkHoEad/mode=imports,min/optimized/three.js"
 
-export default class Game {
-  constructor() {
-    this.scene = new THREE.Scene()
-    this.camera = new THREE.PerspectiveCamera(
-      75,
-      window.innerWidth / window.innerHeight,
-      0.1,
-      1000
-    )
-    this.renderer = new THREE.WebGLRenderer()
-
-    this.renderer.setSize(window.innerWidth, window.innerHeight)
-    document.body.appendChild(this.renderer.domElement)
-    this.addCube()
-    this.addLine()
-    this.camera.position.z = 5
-  }
-
-  addCube() {
+export class Cube {
+  constructor(game) {
     const geometry = new THREE.BoxGeometry()
     const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
     const cube = new THREE.Mesh(geometry, material)
-    this.scene.add(cube)
+    game.scene.add(cube)
     this.cube = cube
+    this.game = game
   }
 
-  addLine() {
+  update() {
+    this.cube.rotation.x += 0.01
+    this.cube.rotation.y += 0.01
+  }
+}
+
+export class Line {
+  constructor(game) {
     const material = new THREE.LineBasicMaterial({ color: 0xff00ff })
-    const points = []
-    points.push(new THREE.Vector3(-1, 0, 0))
-    points.push(new THREE.Vector3(1, 0, 0))
-    points.push(new THREE.Vector3(1, 1, 0))
-    const geometry = new THREE.BufferGeometry().setFromPoints(points)
+    const from = new THREE.Vector3(-1, 0, 0)
+    const to = new THREE.Vector3(1, 1, 0)
+    const geometry = new THREE.BufferGeometry().setFromPoints([from, to])
     const line = new THREE.Line(geometry, material)
-    this.scene.add(line)
+
+    game.scene.add(line)
+    this.line = line
+    this.game = game
+    this.from = from
+    this.to = to
+  }
+
+  update() {}
+}
+
+export default class Game {
+  constructor() {
+    this.scene = new THREE.Scene()
+    const aspectRatio = window.innerWidth / window.innerHeight
+    this.camera = new THREE.PerspectiveCamera(75, aspectRatio, 0.1, 1000)
+    this.renderer = new THREE.WebGLRenderer()
+    this.children = []
+
+    this.renderer.setSize(window.innerWidth, window.innerHeight)
+    document.body.appendChild(this.renderer.domElement)
+  }
+
+  addChild(Class) {
+    this.children.push(new Class(this))
   }
 
   start() {
+    this.addChild(Cube)
+    this.addChild(Line)
+    this.camera.position.z = 5
+
     this.update()
   }
 
   update() {
     requestAnimationFrame(() => this.update())
-    // this.cube.rotation.x += 0.01
-    // this.cube.rotation.y += 0.01
+    for (const child of this.children) child.update()
     this.renderer.render(this.scene, this.camera)
   }
 }
