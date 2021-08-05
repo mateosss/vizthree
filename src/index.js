ca26625ba468dfe6c5c80d1db9015c3e54fd28bc
Mateo de Mayo <mateodemayo@gmail.com>
2021-08-04 23:00:32 -0300

    Add Grid class

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@
 import * as THREE from "https://cdn.skypack.dev/pin/three@v0.131.1-ABR1EJL0AQkCASkHoEad/mode=imports,min/optimized/three.js"
 
 export class Cube {
-  constructor(game) {
+  constructor(game, props) {
     const geometry = new THREE.BoxGeometry()
     const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 })
     const cube = new THREE.Mesh(geometry, material)
@@ -18,21 +18,62 @@ export class Cube {
 }
 
 export class Line {
-  constructor(game) {
-    const material = new THREE.LineBasicMaterial({ color: 0xff00ff })
-    const from = new THREE.Vector3(-1, 0, 0)
-    const to = new THREE.Vector3(1, 1, 0)
-    const geometry = new THREE.BufferGeometry().setFromPoints([from, to])
+  constructor(game, { color = 0xff00ff, from = [-1, 0, 0], to = [1, 1, 0] }) {
+    const material = new THREE.LineBasicMaterial({ color })
+    const a = new THREE.Vector3(...from)
+    const b = new THREE.Vector3(...to)
+    const geometry = new THREE.BufferGeometry().setFromPoints([a, b])
     const line = new THREE.Line(geometry, material)
 
     game.scene.add(line)
-    this.line = line
     this.game = game
+    this.line = line
+
+    this.color = color
     this.from = from
     this.to = to
   }
 
   update() {}
+}
+
+export class Grid {
+  constructor(
+    game,
+    { color = 0xffffff, origin = [-1, -1, 0], extent = [2, 2], spacing = 0.25 }
+  ) {
+    this.children = []
+    const [w, h] = extent
+    const [rows, cols] = [h / spacing, w / spacing]
+    const add2 = ([x, y, z], [a, b]) => [x + a, y + b, z]
+    const makeLine = (from, to) =>
+      this.children.push(new Line(game, { color, from, to }))
+
+    let pointer = origin
+    makeLine(pointer, add2(pointer, [w, 0]))
+    for (let j = 0; j < rows; j++) {
+      pointer = add2(pointer, [0, spacing])
+      makeLine(pointer, add2(pointer, [w, 0]))
+    }
+
+    pointer = origin
+    makeLine(pointer, add2(pointer, [0, h]))
+    for (let i = 0; i < cols; i++) {
+      pointer = add2(pointer, [spacing, 0])
+      makeLine(pointer, add2(pointer, [0, h]))
+    }
+
+    this.game = game
+
+    this.color = color
+    this.origin = origin
+    this.extent = extent
+    this.spacing = spacing
+  }
+
+  update() {
+    for (const child of this.children) child.update()
+  }
 }
 
 export default class Game {
@@ -48,12 +89,13 @@ export default class Game {
   }
 
   addChild(Class) {
-    this.children.push(new Class(this))
+    this.children.push(new Class(this, {}))
   }
 
   start() {
     this.addChild(Cube)
-    this.addChild(Line)
+    // this.addChild(Line)
+    this.addChild(Grid)
     this.camera.position.z = 5
 
     this.update()
